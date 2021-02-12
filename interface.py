0c26594a1eef898835862777982edce90803a1a8
lilimejb <sibanovvasilij@gmail.com>
2021-02-12 17:03:56 +0700

    lul

---
+++ interface.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
+
+
+class MainWin(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWin()
+    ex.show()
+    sys.exit(app.exec_())
