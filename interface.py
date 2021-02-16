d1dd9024f7736b8c3d87eb062423cb55bc30fded
Artyom Troyazykov <mrheldit2@gmail.com>
2021-02-16 17:38:42 +0700

    update the shit

--- interface.py
+++ interface.py
@@ -1,7 +1,7 @@
 import sys
-import requests
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -13,22 +13,64 @@ class MainWin(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)
         self.maps = Maps()
-        self.image = None
-        self.place = None
+        self.image = self.place = self.layer = None
 
         self.button_connect()
+        self.setFocus()
 
     def button_connect(self):
         self.find_button.clicked.connect(self.find_place)
+        self.layer_chooser.currentTextChanged.connect(self.find_place)
 
     def find_place(self):
-        self.place = (self.longitude.text(), self.latitude.text())
-        self.image = QPixmap(self.maps.getImage(*self.place))
+        self.layer = self.layer_chooser.currentText()
+        if self.layer == 'Схема':
+            self.layer = 'map'
+        if self.layer == 'Спутник':
+            self.layer = 'sat'
+        if self.layer == 'Гибрид':
+            self.layer = 'sat,skl'
+        if self.longitude.text() and self.lattitude.text():
+            self.place = (self.longitude.text(), self.latitude.text())
+            self.image = QPixmap(self.maps.getImage(c1=self.place[0], c2=self.place[1], layer=self.layer))
+        if self.geodata_text.text():
+            self.place = self.geodata_text.text()
+            self.image = QPixmap(self.maps.getImage(geo=self.place, layer=self.layer))
         self.map_label.setPixmap(self.image)
+        self.setFocus()
+
+    def keyPressEvent(self, event):
+        keyD = False
+        speed = 0.002
+        if event.key() == Qt.Key_PageUp:
+            self.maps.spn += speed
+            keyD = True
+        if event.key() == Qt.Key_PageDown:
+            self.maps.spn -= speed
+            keyD = True
+        if event.key() == Qt.Key_Up:
+            self.maps.pos[1] += self.maps.spn * 2
+            keyD = True
+        if event.key() == Qt.Key_Down:
+            self.maps.pos[1] -= self.maps.spn * 2
+            keyD = True
+        if event.key() == Qt.Key_Left:
+            self.maps.pos[0] -= self.maps.spn * 2
+            keyD = True
+        if event.key() == Qt.Key_Right:
+            self.maps.pos[0] += self.maps.spn * 2
+            keyD = True
+        if keyD:
+            self.find_place()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWin()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec_())
--- maps.py
+++ maps.py
@@ -1,18 +1,42 @@
 import os
-
 import requests
 
 
 class Maps:
     def __init__(self):
-        pass
+        self.pos = [0, 0]
+        self.spn = 0.002
+        self.pt = {}
 
-    def getImage(self, c1, c2):
+    def getImage(self, c1=0, c2=0, geo=0, layer=''):
         res_path = "temp/tmp.png"
         if not os.path.isdir("temp"):
             os.mkdir("temp")
+        
+        pt = '&pt='
+        for i in self.pt.items():
+            pt += f'{i[1][0]},{i[1][1]},pmwtm{i[0]}~'
+        pt = pt[:-1]
 
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={c1},{c2}&spn=0.002,0.002&l=map"
+        if c1 != 0 and c2 != 0:
+            if (c1, c2) not in self.pt.values():
+                self.pt[1] = (c1, c2)
+                return self.getImage(c1, c2, geo, layer)
+            map_request = f"http://static-maps.yandex.ru/1.x/?ll={float(c1) + self.pos[0]},{float(c2) + self.pos[1]}" \
+                          f"&spn={self.spn},{self.spn}&l={layer}" + pt
+        if geo != 0:
+            geo = '+'.join(geo.split())
+            geocoder = f'https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={geo}&format=json'
+            response = requests.get(geocoder)
+            geocoder = response.json()
+            toponym = geocoder["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            splitted = toponym_coodrinates.split()
+            x = splitted[0]
+            y = splitted[1]
+            if (x, y) not in self.pt.values():
+                self.pt[1] = (x, y)
+                return self.getImage(x, y, geo, layer)
         response = requests.get(map_request)
 
         with open(res_path, "wb") as file:
