3c67b47bac9be494fbab8f2bb1d531749d887146
Artyom Troyazykov <mrheldit2@gmail.com>
2021-02-16 16:45:29 +0700

    maps update

--- maps.py
+++ maps.py
@@ -7,13 +7,13 @@ class Maps:
         self.pos = [0, 0]
         self.spn = 0.002
 
-    def getImage(self, c1, c2, layer):
+    def getImage(self, c1, c2, layer, pt=None):
         res_path = "temp/tmp.png"
         if not os.path.isdir("temp"):
             os.mkdir("temp")
 
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={float(c1) + self.pos[0]},{float(c2) + self.pos[1]}" \
-                      f"&spn={self.spn},{self.spn}&l={layer}"
+                      f"&spn={self.spn},{self.spn}&l={layer}" + (f"&pt={pt[0]},{pt[1]}" if pt else "")
         response = requests.get(map_request)
 
         with open(res_path, "wb") as file:
